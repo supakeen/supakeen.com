417b1b09fef99d18b8dc421624437316f703ef47
supakeen <cmdr@supakeen.com>
2018-09-10 10:23:31 +0200

    generated new html

--- publish.py
+++ publish.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import json
+import re
 
 path = "."
 
 def main():
     targets = sys.argv[1:]
+
+    if not len(targets):
+        targets = ["github", "ipns"]
+
+    if "ipns" in targets:
+        site = subprocess.check_output(["ipfs", "add", "-r", path])
+        site = site.splitlines()[-1].split()[1]
+        subprocess.check_output(["ipfs", "name", "publish", site])
+
+        # TODO this is obviously not easy to solve and will always point to the
+        # TODO old content hash for the site, however do people on IPFS and are
+        # TODO already on the site really want to confirm??
+
+        with open("index.html", "r") as handle:
+            index = handle.read()
+
+        index = re.sub("IPFS <em>([a-zA-Z0-9]+)</em>", "IPFS <em>{}</em>".format(site.decode("ascii")), index)
+
+        print(index)
+
+        with open("index.html", "w") as handle:
+            handle.write(index)
 
     if "github" in targets:
         # Do a pretty straightforward git push first
