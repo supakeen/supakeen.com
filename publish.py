3967ff9288b4ee93c8f18348669c892925b91a0d
supakeen <cmdr@supakeen.com>
2018-09-10 09:39:34 +0200

    generated new html

--- publish.py
+++ publish.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 import subprocess
+import sys
 
 path = "."
 
-subprocess.check_output(["git", "add", path])
-subprocess.check_output(["git", "commit", "-m", "generated new html"])
+def main():
+    targets = sys.argv[1:]
+
+    if "github" in targets:
+        # Do a pretty straightforward git push first
+        subprocess.check_output(["git", "add", path])
+        subprocess.check_output(["git", "commit", "-m", "generated new html"])
+        subprocess.check_output(["git", "push"])
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
