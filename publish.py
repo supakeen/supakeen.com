a01eea93b01329d54f5ecbb889980b716430519b
supakeen <cmdr@supakeen.com>
2018-11-18 14:53:53 +0100

    Remove IPNS, it's unused.

--- publish.py
+++ publish.py
@@ -12,29 +12,6 @@ def main():
     if not len(targets):
         targets = ["github", "ipns"]
 
-    if "ipns" in targets:
-        print("Publishing to IPFS")
-        site = subprocess.check_output(["ipfs", "add", "-r", path])
-        site = site.splitlines()[-1].split()[1]
-        print("Content hash", site.decode("ascii"))
-        print("Updating ipns name")
-        subprocess.check_output(["ipfs", "name", "publish", site])
-
-        # TODO this is obviously not easy to solve and will always point to the
-        # TODO old content hash for the site, however do people on IPFS and are
-        # TODO already on the site really want to confirm??
-
-        print("Rewriting index")
-        with open("index.html", "r") as handle:
-            index = handle.read()
-
-        index = re.sub("IPFS <em>([a-zA-Z0-9]+)</em>", "IPFS <em>{}</em>".format(site.decode("ascii")), index)
-
-        print(index)
-
-        with open("index.html", "w") as handle:
-            handle.write(index)
-
     if "github" in targets:
         # Do a pretty straightforward git push first
         subprocess.check_output(["git", "add", path])
